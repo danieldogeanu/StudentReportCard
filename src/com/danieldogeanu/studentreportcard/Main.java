8b25b71e4fa0a32870ea81bd9335317fab8081c8
Daniel Dogeanu <contact@danieldogeanu.com>
2018-01-12 15:46:07 +0200

    Added ArrayList of ReportCards for All Students

--- src/com/danieldogeanu/studentreportcard/Main.java
+++ src/com/danieldogeanu/studentreportcard/Main.java
@@ -6,18 +6,26 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ReportCard studentOne = new ReportCard();
-        studentOne.setStudentName("John Doe");
-        studentOne.setEnglishGrade(9);
-        studentOne.setMathGrade(7);
-        studentOne.setHistoryGrade(5);
-        studentOne.setBiologyGrade(10);
-        studentOne.setGeographyGrade(3);
-        studentOne.setChemistryGrade(8);
-        studentOne.setPhysicsGrade(9);
+        ReportCard johnDoe = new ReportCard();
+        johnDoe.setStudentName("John Doe");
+        johnDoe.setEnglishGrade(9);
+        johnDoe.setMathGrade(7);
+        johnDoe.setHistoryGrade(5);
+        johnDoe.setBiologyGrade(10);
+        johnDoe.setGeographyGrade(3);
+        johnDoe.setChemistryGrade(8);
+        johnDoe.setPhysicsGrade(9);
 
-        System.out.println(studentOne.toString());
-        System.out.println("----");
+        ArrayList<ReportCard> students = new ArrayList<ReportCard>();
+        students.add(johnDoe);
+        students.add(new ReportCard("Lettice Kaylie", 7, 8, 5, 6, 10, 8, 9));
+        students.add(new ReportCard("Jazlyn Eli", 9, 7, 9, 6, 6, 8, 7));
+        students.add(new ReportCard("Wallace Renita", 4, 3, 5, 2, 6, 8, 3));
+        students.add(new ReportCard("Andrea Katharyn", 6, 8, 8, 9, 3, 10, 7));
+
+        for (ReportCard student : students) {
+            System.out.println(student.toString());
+        }
 
     }
 }
